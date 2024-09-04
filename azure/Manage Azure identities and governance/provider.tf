e33cb1419442819b171a275930cda28c10b7a6c5
Chafe Solomon <62526967+ChafeSolomon@users.noreply.github.com>
2024-09-04 05:43:48 -0400

    Update provider.tf

--- azure/Manage Azure identities and governance/provider.tf
+++ azure/Manage Azure identities and governance/provider.tf
@@ -15,13 +15,11 @@ terraform {
     storage_account_name = "az104tfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc = true
 }
 }
 
 # Configure the Azure Resource Manager Provider
 provider "azurerm" {
-  use_oidc = true
   features {}
 }
 
